ce70272baddf788bb11f0ae9505f8ad7cff63797
cyrus01337 <cyrus21337@gmail.com>
2023-12-18 13:02:12 +0000

    Add workspace file generation

--- __main__.py
+++ __main__.py
@@ -1,8 +1,25 @@
 #!/usr/bin/env python3
 import argparse
+import copy
+import json
 import pathlib
+import typing
 
 
+# TODO: Move to module
+class WorkspacePath(typing.TypedDict):
+    path: str
+
+
+class WorkspaceTemplate(typing.TypedDict):
+    folders: list[WorkspacePath]
+    settings: dict
+
+
+WORKSPACE_TEMPLATE = WorkspaceTemplate(folders=[WorkspacePath(path="")], settings={})
+
+
+# TODO: Move to module
 # TODO: Use __slots__
 class Namespace:
     directories: list[pathlib.Path]
@@ -40,10 +57,34 @@ PARSER.add_argument(
 PARSER.add_argument("--work", action="store_true", required=False, help="Generate in work sub-directory")
 
 
-def main():
-    args = PARSER.parse_args(namespace=Namespace())
+# TODO: Determine using TOML config
+def get_target_directory(args: Namespace):
+    target_directory = pathlib.Path.home() / "Workspaces"
+    sub_directory: str = ""
 
-    print(args)
+    if args.cli:
+        sub_directory = "cli"
+    elif args.fork:
+        sub_directory = "fork"
+    elif args.third_party:
+        sub_directory = "third-party"
+    elif args.work:
+        sub_directory = "work"
+
+    return target_directory / sub_directory if sub_directory else target_directory
+
+
+def main():
+    args: Namespace = PARSER.parse_args(namespace=Namespace())
+    target_directory = get_target_directory(args)
+
+    for directory in map(pathlib.Path.resolve, args.directories):
+        workspace = copy.deepcopy(WORKSPACE_TEMPLATE)
+        workspace["folders"][0]["path"] = str(directory.relative_to(target_directory, walk_up=True))
+        workspace_file = target_directory / f"{directory.name}.code-workspace"
+
+        with workspace_file.open("w") as fh:
+            json.dump(workspace, fh, indent=4, sort_keys=True)
 
 
 if __name__ == "__main__":
