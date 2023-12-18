8dc3851f56214a679f760a6854b8e91c3267896e
cyrus01337 <cyrus21337@gmail.com>
2023-12-18 15:23:13 +0000

    Modularised CLI argument parsing

--- __main__.py
+++ __main__.py
@@ -1,55 +1,14 @@
 #!/usr/bin/env python3
-import argparse
 import pathlib
 
-from src import workspaces
+from src import arguments, workspaces
 
-
-# TODO: Move to module
-# TODO: Use __slots__
-class Namespace:
-    directories: list[pathlib.Path]
-    cli: bool
-    fork: bool
-    third_party: bool
-    work: bool
-
-    # TODO: Refactor?
-    def __str__(self):
-        return (
-            "<Namespace "
-            f"directories={self.directories}"
-            f"{' cli' if self.cli else ''}"
-            f"{' fork' if self.fork else ''}"
-            f"{' third_party' if self.third_party else ''}"
-            f"{' work' if self.work else ''}"
-            ">"
-        )
-
-
-CWD = pathlib.Path.cwd()
-PARSER = argparse.ArgumentParser(
-    prog="create-workspace",
-    description="CLI tool for creating workspaces using my personal environment",
-    add_help=True,
-    allow_abbrev=True,
-)
-PARSER.add_argument("directories", nargs="*", type=pathlib.Path, default=[CWD], help="Generate in CLI sub-directory")
-# TODO: Assign sub-directory name as default value for all sub-directory flags
-# and reflect in get_target_directory
-#
-# https://docs.python.org/3/library/argparse.html#action
-PARSER.add_argument("--cli", action="store_true", required=False, help="Generate in CLI sub-directory")
-PARSER.add_argument("--fork", action="store_true", required=False, help="Generate in forks sub-directory")
-PARSER.add_argument(
-    "--third-party", action="store_true", dest="third_party", required=False, help="Generate in third-party sub-directory"
-)
-PARSER.add_argument("--work", action="store_true", required=False, help="Generate in work sub-directory")
+HOME = pathlib.Path.home()
 
 
 # TODO: Determine using TOML config
-def get_target_directory(args: Namespace):
-    target_directory = pathlib.Path.home() / "Workspaces"
+def get_target_directory(args: arguments.Arguments):
+    target_directory = HOME / "Workspaces"
     sub_directory: str = ""
 
     if args.cli:
@@ -65,7 +24,7 @@ def get_target_directory(args: Namespace):
 
 
 def main():
-    args: Namespace = PARSER.parse_args(namespace=Namespace())
+    args = arguments.parse()
     target_directory = get_target_directory(args)
 
     for directory in map(pathlib.Path.resolve, args.directories):
---
+++ src/arguments.py
@@ -0,0 +1,47 @@
+import argparse
+import pathlib
+
+
+# TODO: Use __slots__
+class Arguments:
+    directories: list[pathlib.Path]
+    cli: bool
+    fork: bool
+    third_party: bool
+    work: bool
+
+    # TODO: Refactor?
+    def __str__(self):
+        return (
+            "<Namespace "
+            f"directories={self.directories}"
+            f"{' cli' if self.cli else ''}"
+            f"{' fork' if self.fork else ''}"
+            f"{' third_party' if self.third_party else ''}"
+            f"{' work' if self.work else ''}"
+            ">"
+        )
+
+
+_CWD = pathlib.Path.cwd()
+_PARSER = argparse.ArgumentParser(
+    prog="create-workspace",
+    description="CLI tool for creating workspaces using my personal environment",
+    add_help=True,
+    allow_abbrev=True,
+)
+_PARSER.add_argument("directories", nargs="*", type=pathlib.Path, default=[_CWD], help="Generate in CLI sub-directory")
+# TODO: Assign sub-directory name as default value for all sub-directory flags
+# and reflect in get_target_directory
+#
+# https://docs.python.org/3/library/argparse.html#action
+_PARSER.add_argument("--cli", action="store_true", required=False, help="Generate in CLI sub-directory")
+_PARSER.add_argument("--fork", action="store_true", required=False, help="Generate in forks sub-directory")
+_PARSER.add_argument(
+    "--third-party", action="store_true", dest="third_party", required=False, help="Generate in third-party sub-directory"
+)
+_PARSER.add_argument("--work", action="store_true", required=False, help="Generate in work sub-directory")
+
+
+def parse():
+    return _PARSER.parse_args(namespace=Arguments())
